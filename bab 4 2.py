d17ba6a1ffdb037c47635c7aec682a18283a037b
azkagenta <gentaazka24@gmail.com>
2024-06-06 17:28:36 +0700

    Add files via upload

---
+++ bab 4 2.py
@@ -0,0 +1,8 @@
+kampus = 'UMS'
+lokasi = 'Surakarta'
+print('Selamat Datang di {}, kampus {} tercinta'.format(lokasi,kampus))
+
+string = "nilai {1} {0}, adalah {2}%"
+print(string.format('Irwan', 'Arthur', 75))
+
+string_dua = '{band} menciptakan lagu {lagu} pada {tahun}'
