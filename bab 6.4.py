ed33428c2a020676fd26678e5a0800dc19842707
azkagenta <gentaazka24@gmail.com>
2024-06-14 22:21:27 +0700

    Add files via upload

---
+++ bab 6.4.py
@@ -0,0 +1,13 @@
+mahasiswa = {'nama':'Andika','umur':21}
+mahasiswa['umur'] = 19
+mahasiswa['alamat'] = 19
+mahasiswa['angkatan'] = 'Sragen'
+print(mahasiswa) 
+print(mahasiswa.pop('angkatan'))
+print(mahasiswa)
+print('nama' in mahasiswa)
+print(len(mahasiswa))
+print(sorted(mahasiswa))
+mahasiswa.clear()
+print(mahasiswa)
+     
