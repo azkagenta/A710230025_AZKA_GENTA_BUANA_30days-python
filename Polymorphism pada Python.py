d2ab4cf6e82daea3e9625a0ef55fab0fa99c0f49
azkagenta <gentaazka24@gmail.com>
2024-06-27 08:14:48 +0700

    Add files via upload

---
+++ Polymorphism pada Python.py
@@ -0,0 +1,11 @@
+my_string = "Hello, World!"    # Assigns a string to variable my_string
+my_list = [1, 2, 3, 4, 5]      # Assigns a list to variable my_list
+my_tuple = (6, 7, 8, 9, 10)    # Assigns a tuple to variable my_tuple
+my_set = {11, 12, 13, 14, 15}  # Assigns a set to variable my_set
+my_dict = {"name": "John", "age": 30, "city": "New York"}  # Assigns a dictionary to variable my_dict
+
+print(len(my_string))  # Outputs: 13 (length of the string "Hello, World!")
+print(len(my_list))    # Outputs: 5 (number of elements in the list [1, 2, 3, 4, 5])
+print(len(my_tuple))   # Outputs: 5 (number of elements in the tuple (6, 7, 8, 9, 10))
+print(len(my_set))     # Outputs: 5 (number of elements in the set {11, 12, 13, 14, 15})
+print(len(my_dict))    # Outputs: 3 (number of key-value pairs in the dictionary {"name": "John", "age": 30, "city": "New York"})
